09a432448251d067afc2fafca94f980cfc945fef
Kanadets <fomindmitrii@icloud.com>
2020-02-29 18:15:50 -0800

    Started Project Smart-Brains, added first components and styled them

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Navigation from "./Components/Navigation/Navigation";
+import Logo from "./Components/Logo/Logo";
+import Rank from "./Components/Rank/Rank";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm.js";
+import Particles from "react-particles-js";
+import "./App.css";
+import "tachyons";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
