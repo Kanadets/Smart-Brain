548a1ebb88358b57478782294a2e53f198548b11
Kanadets <fomindmitrii@icloud.com>
2020-02-29 20:05:12 -0800

    Added Clarifai API and I made it recognise face and draw box

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import Rank from "./Components/Rank/Rank";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm.js";
+import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import "./App.css";
 import "tachyons";
+
+const app = new Clarifai.App({
+  apiKey: "74e3cc71c62341e2bcfbcd74c8ff4503"
+});
 
 const particlesOptions = {
   particles: {
@@ -18,7 +24,50 @@ const particlesOptions = {
     }
   }
 };
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {}
+    };
+  }
+
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box: box });
+  };
+
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
       <div className="App">
@@ -26,8 +75,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
